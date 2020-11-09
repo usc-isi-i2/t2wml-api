c213abe52c4c2232bea23e222a1ab082a1ff18ae
devora <devorawitty@gmail.com>
2020-11-09 14:57:31 +0200

    remove unused import

--- t2wml/mapping/statements.py
+++ t2wml/mapping/statements.py
@@ -4,7 +4,7 @@ from t2wml.parsing.t2wml_parsing import iter_on_n_for_code, T2WMLCode
 from t2wml.parsing.classes import ReturnClass
 from t2wml.spreadsheets.conversions import to_excel
 from t2wml.wikification.utility_functions import get_property_type
-from t2wml.utils.utilities import VALID_PROPERTY_TYPES, parse_datetime, translate_precision_to_integer
+from t2wml.utils.utilities import VALID_PROPERTY_TYPES, parse_datetime
 from t2wml.settings import t2wml_settings
 
 class Node:
