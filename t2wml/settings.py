ff6e1b7924f5d50e5721f914ccfb4374c5f53ebc
devora <devorawitty@gmail.com>
2020-11-15 15:44:04 +0200

    add handle_calendar setting, and update_from_dict function

--- t2wml/settings.py
+++ t2wml/settings.py
@@ -8,6 +8,8 @@ class T2WMLSettings:
         self.sparql_endpoint=DEFAULT_SPARQL_ENDPOINT
         self.wikidata_provider=None #default is DictionaryProvider with preloaded properties
         self.warn_for_empty_cells=False
+        self.handle_calendar="leave"
+
     @property
     def cache_data_files(self):
         if self.cache_data_files_folder is not None:
@@ -16,6 +18,11 @@ class T2WMLSettings:
             else:
                 raise ValueError("Cache folder in settings does not exist: "+self.cache_data_files_folder)
         return False
+
+    def update_from_dict(self, **kwargs):
+        for key in self.__dict__:
+            if key in kwargs:
+                self.__dict__[key]=kwargs[key]
 
 
 t2wml_settings=T2WMLSettings()
