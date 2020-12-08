8fcdcf5ebc777b62212abdfc4701f575c70d455c
devora <devorawitty@gmail.com>
2020-12-08 15:45:56 +0200

    add qualifier support, clean up

--- t2wml/input_processing/annotation_parsing.py
+++ t2wml/input_processing/annotation_parsing.py
@@ -1,8 +1,8 @@
 import json
 import numpy as np
 from munkres import Munkres
-from t2wml.spreadsheets.conversions import cell_tuple_to_str, cell_range_str_to_tuples, cell_str_to_tuple, column_index_to_letter
-
+from t2wml.spreadsheets.conversions import cell_tuple_to_str, column_index_to_letter
+COST_MATRIX_DEFAULT = 5
 
 class YamlFormatter:
     @staticmethod
@@ -19,199 +19,41 @@ statementMapping:
             {qualifierLines}""".format(region=region, mainSubjectLine=mainSubjectLine, propertyLine=propertyLine, optionalsLines=optionalsLines, qualifierLines=qualifierLines)
         return yaml
     
-
-
-
-class ValueArgsOld:
-    def __init__(self, range_str, use_item=False):
-        self.range_str=range_str
-        self.use_item=use_item
-        self.cell_args=self.get_cell_args(range_str)
-        self.property_alignment=None
+    @staticmethod
+    def get_qualifier_region_string(left, right, top, bottom):         
+        region="""left: {left}
+                right: {right}
+                top: {top}
+                bottom: {bottom}""".format(left=left, right=right, top=top, bottom=bottom)
+        return region
     
-    def get_cell_args(self, range_str):
-        if ":" in range_str:
-            return cell_range_str_to_tuples(range_str)
-        try:
-            tuple=cell_str_to_tuple(range_str)
-            return tuple, tuple
-        except:
-            return (None, None), (None, None)
-    
-    @property
-    def col_args(self):
-        return self.cell_args[0][0], self.cell_args[1][0]
-    
-    @property
-    def row_args(self):
-        return self.cell_args[0][1], self.cell_args[1][1]
-    
-    @property
-    def is_2D(self):
-        if self.cell_args[0][0] == self.cell_args[1][0]:
-            return False
-        if self.cell_args[0][1] == self.cell_args[1][1]:
-            return False
-        return True
-
-
-    def __repr__(self):
-        return self.range_str + "::" + str(self.property_alignment)
-    
-    def __str__(self):
-        return self.__repr__()
-    
-    def get_alignment(self, relative_args):
-        if self.row_args==relative_args.row_args:
-            return "row"
-        if self.col_args==relative_args.col_args:
-            return "col"
-        return False
-    
-    def get_expression(self, relative_value_args, use_q=False):
-        if self.use_item:
-            return_string= "=item[{indexer}]"
-        else:
-            return_string= "=value[{indexer}]"
-        
-        if self.cell_args[0] == self.cell_args[1]: #single cell
-            cell_str = column_index_to_letter(self.cell_args[0][0]) + ", " + str(self.cell_args[0][1]+1)
-            return return_string.format(indexer=cell_str)
-        
-        row_var = ", $qrow-$n" if use_q else ", $row-$n"
-        col_var= "$qcol-$n, " if use_q else "$col-$n, "
-        
-        if self.get_alignment(relative_value_args)=="row":
-            col=column_index_to_letter(self.cell_args[0][0])
-            return return_string.format(indexer=col+row_var)
-
-        elif self.get_alignment(relative_value_args)=="col":
-            row=str(self.cell_args[0][1]+1)
-            return return_string.format(indexer=col_var+row)
-        else:
-            print("Don't know how to match with imperfect alignment yet"+self.range_str +","+relative_value_args.range_str)
-            return "#TODO: ????? -Don't know how to match with imperfect alignment yet"
-    
-
-
-
-class Annotations:
-    def __init__(self, data_regions, subject_regions, qualifier_regions=[], property_regions=[], sheet=None):
-        self.data_regions=data_regions
-        self.subject_regions=subject_regions
-        self.qualifier_regions=qualifier_regions or []
-        self.property_regions= property_regions or []
-        self.sheet=sheet
-        self.find_alignments()
-    
-    def find_alignments(self):
-        num_properties=len(self.property_regions)
-        num_to_assign=len(self.data_regions)+len(self.qualifier_regions)
-        cost_matrix= np.empty((num_properties, num_to_assign), dtype=int)
-        cost_matrix.fill(5)
-        alignment_arr=[dat for dat in self.data_regions]+[qual for qual in self.qualifier_regions]
-
-        for p_i, p_reg in enumerate(self.property_regions):
-            for d_i, d_reg in enumerate(self.data_regions):
-                if p_reg.get_alignment(d_reg):
-                    cost_matrix[p_i][d_i]=1
-                
-            for q_i, q_reg in enumerate(self.qualifier_regions):
-                if p_reg.get_alignment(q_reg):
-                    cost_matrix[p_i][q_i+len(self.data_regions)]=1
-        
-        m=Munkres()
-        indexes=m.compute(cost_matrix)
-        not_matched=[True]*num_to_assign
-        for (p_i, x_i) in indexes:
-            not_matched[x_i]=False
-            alignment_arr[x_i].property_alignment=self.property_regions[p_i]
-        
-        return indexes
-
-    def get_yaml_parts(self, input_region, use_q=False):
-        propertyLine=optionalsLines=""
-        if input_region.property_alignment:
-            propertyLine = input_region.property_alignment.get_expression(input_region, use_q)
-        else: 
-            propertyLine = "P2561  #??? #TODO-- no property alignment found"
-            #TODO: can add heuristics here like P585 for points in time
-        return propertyLine, optionalsLines
-    
-    def get_qualifier_yaml(self, qualifier_region: ValueArgsOld, data_region):
-        propertyLine, optionalsLines = self.get_yaml_parts(qualifier_region, use_q=True)
-        if qualifier_region.is_2D:
-            if qualifier_region.use_item:
-                valueLine="=item[$qcol, $qrow]"
-            else:
-                valueLine="=value[$qcol, $qrow]"
-            
-            alignment = qualifier_region.get_alignment(data_region)
-            if alignment == False:
-                region="range: "+qualifier_region.range_str
-            else:
-                if alignment == "col":
-                    left=right="=$col"
-                    top, bottom =qualifier_region.row_args
-                    top+=1
-                    bottom+=1
-
-                else: # alignment == "row":
-                    top=bottom="=$row"
-                    left, right = qualifier_region.col_args
-                    left, right = column_index_to_letter(left), column_index_to_letter(right)
-
-            
-                region="""left: {left}
-                        right: {right}
-                        top: {top}
-                        bottom: {bottom}""".format(left=left, right=right, top=top, bottom=bottom)
-            
-
-
+    @staticmethod
+    def get_qualifier_string(propertyLine, optionalsLines, valueLine, region=None):
+        if region is not None:
             qualifier_string = """
                 - region: 
                         {region}
                   property: {propertyLine}
-                  value: =value[$qcol, $qrow]
+                  value: {valueLine}
                 {optionalsLines}""".format(region=region, propertyLine=propertyLine, valueLine=valueLine, optionalsLines=optionalsLines)
-
         else:
-            valueLine=qualifier_region.get_expression(data_region)
             qualifier_string = """
                 - property: {propertyLine}
                   value: {valueLine}
                 {optionalsLines}""".format(propertyLine=propertyLine, valueLine=valueLine, optionalsLines=optionalsLines)
         return qualifier_string
 
-    def generate_yaml(self):
-        yamls=[]
-        for data_region in self.data_regions:
-            for subject_region in self.subject_regions:
-                region="range: {range_str}".format(range_str=data_region.range_str)
-                propertyLine, optionalsLines = self.get_yaml_parts(data_region)
-                mainSubjectLine=subject_region.get_expression(data_region)
-                if len(self.qualifier_regions):
-                    qualifierLines="qualifier:"
-                    for qualifier in self.qualifier_regions:
-                        qualifierLines+=self.get_qualifier_yaml(qualifier, data_region)
-                else:
-                    qualifierLines=""
-                yaml=YamlFormatter.get_yaml_string(region, mainSubjectLine, propertyLine, optionalsLines, qualifierLines)
-                yamls.append(yaml)
-        return yamls
 
-
-COST_MATRIX_DEFAULT = 5
 
 
 class ValueArgs:
     def __init__(self, annotation):
         self.annotation=annotation
+        self.role = annotation["role"]
         self.selection= annotation["selections"][0]
         self.use_item= annotation["type"]=="qNode"
         self.cell_args=self.get_cell_args(self.selection)
-        self.match_instance=None
+        self.matches={}
         self.match_found=False
     
     def get_cell_args(self, selection):
@@ -299,14 +141,13 @@ class DynamicallyGeneratedAnnotation:
         self.qualifier_annotations= qualifier_annotations or []
         self.metadata_annotations= metadata_annotations or []
         self.property_annotations= property_annotations or []
-    
+
     def _run_cost_matrix(self, match_candidates, targets_collection):
+        if not len(match_candidates):
+            return
         match_targets = []
         for arr in targets_collection:
             match_targets+=arr
-        
-        match_candidates=[ValueArgs(c) for c in match_candidates]
-        match_targets=[ValueArgs(t) for t in match_targets]
 
         cost_matrix= np.empty((len(match_candidates), len(match_targets)), dtype=int)
         cost_matrix.fill(COST_MATRIX_DEFAULT)
@@ -317,34 +158,74 @@ class DynamicallyGeneratedAnnotation:
         m=Munkres()
         indexes=m.compute(cost_matrix)
         for (c_i, t_i) in indexes:
-            match_targets[t_i].match_instance=match_candidates[c_i]
+            match_vector = match_candidates[c_i].role
+            match_targets[t_i].matches[match_vector]=match_candidates[c_i]
             match_candidates[c_i].match_found=True
+    
+    def get_optionals_and_property(self, region, use_q):
+        property=region.matches.get("property", None)
+        if property  is None:
+            propertyLine = "#TODO-- no property alignment found"
+        else:
+            propertyLine = property.get_expression(region, use_q)
         
-        return match_candidates, match_targets
+        optionalsLines=""
+        for key in region.matches:
+            if key!="property":
+                optionalsLines+=key + ": " + region.matches[key].get_expression(region, use_q) + "\n"
+        
+        return propertyLine, optionalsLines
 
+    def _get_qualifier_yaml(self, qualifier_region, data_region):
+        propertyLine, optionalsLines = self.get_optionals_and_property(qualifier_region, use_q=True)
+        region=None
 
-    def _generate_yaml(self, data_annotation, subject_annotation):
-        subject_region=ValueArgs(subject_annotation)
-        data_region=ValueArgs(data_annotation)
+        if qualifier_region.is_2D:
+            if qualifier_region.use_item:
+                valueLine="=item[$qcol, $qrow]"
+            else:
+                valueLine="=value[$qcol, $qrow]"
+            
+            alignment = qualifier_region.get_alignment(data_region)
+            if alignment == False:
+                region="range: "+qualifier_region.range_str
+            else:
+                if alignment == "col":
+                    left=right="=$col"
+                    top, bottom =qualifier_region.row_args
+                    top+=1
+                    bottom+=1
+
+                else: # alignment == "row":
+                    top=bottom="=$row"
+                    left, right = qualifier_region.col_args
+                    left, right = column_index_to_letter(left), column_index_to_letter(right)
+                
+                region=YamlFormatter.get_qualifier_region_string(left, right, top, bottom)
+        else:
+            valueLine=qualifier_region.get_expression(data_region)
+        
+        qualifier_string=YamlFormatter.get_qualifier_string(propertyLine, optionalsLines, valueLine, region)
+            
+        return qualifier_string
+
+    def _generate_yaml(self, data_region, subject_region, qualifier_regions):
         region="range: {range_str}".format(range_str=data_region.range_str)
         mainSubjectLine=subject_region.get_expression(data_region)
 
-        if self.property_annotations:
-            property_candidates, property_targets=self._run_cost_matrix(self.property_annotations, [[data_annotation], self.qualifier_annotations])
-            if property_targets[0].match_instance is None:
-                propertyLine = "P2561  #??? #TODO-- no property alignment found"
-            else:
-                propertyLine = property_targets[0].match_instance.get_expression(property_targets[0], use_q=False)
+        propertyLine, optionalsLines = self.get_optionals_and_property(data_region, use_q=False)
+
+        if len(qualifier_regions):
+            qualifierLines="qualifier:"
+            for qualifier in qualifier_regions:
+                qualifierLines+=self._get_qualifier_yaml(qualifier, data_region)
         else:
-            propertyLine = "#TODO-- no property arguments provided"
-        
-        optionalsLines=""
-        qualifierLines=""
+            qualifierLines=""
         
         yaml=YamlFormatter.get_yaml_string(region, mainSubjectLine, propertyLine, optionalsLines, qualifierLines)
         return yaml
     
-    def generate_yaml(self):
+    def generate_yaml(self, sheet=None):
         return_string=""
         
         #check if no point in generating yet.
@@ -354,12 +235,20 @@ class DynamicallyGeneratedAnnotation:
             return_string+="# cannot create yaml without a main subject\n"
         if return_string:
             return [return_string]
-    
+        
+        data_regions=[ValueArgs(d) for d in self.data_annotations]
+        subject_regions=[ValueArgs(s) for s in self.subject_annotations]
+        qualifier_regions=[ValueArgs(q) for q in self.qualifier_annotations]
 
+        property_regions=[ValueArgs(p) for p in self.property_annotations]
+        metadata_regions=[ValueArgs(m) for m in self.metadata_annotations]
+        self._run_cost_matrix(property_regions, [data_regions, qualifier_regions])
+        self._run_cost_matrix(metadata_regions, [data_regions, qualifier_regions])
+        
         return_arr=[]
-        for data_region in self.data_annotations:
-            for subject_region in self.subject_annotations:
-                yaml = self._generate_yaml(data_region, subject_region)
+        for data_region in data_regions:
+            for subject_region in subject_regions:
+                yaml = self._generate_yaml(data_region, subject_region, qualifier_regions)
                 return_arr.append(yaml)
         return return_arr
     
