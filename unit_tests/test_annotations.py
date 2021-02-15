186650cf88a2acd22a37463168b9e92a742ea124
devora <devorawitty@gmail.com>
2021-02-15 12:52:45 +0200

    add tests

--- unit_tests/test_annotations.py
+++ unit_tests/test_annotations.py
@@ -1,4 +1,5 @@
 import unittest
+import json
 from pathlib import Path
 from t2wml.input_processing.annotation_parsing import Annotation, AnnotationNodeGenerator
 
@@ -11,6 +12,19 @@ class TestAnnotations(unittest.TestCase):
         anno=Annotation.load(anno_path)
         anno.initialize()
         anno.save(anno_path)
+        
+    def test_user_link(self):
+        with open(unit_test_folder/"test_annotation.annotation", 'r') as f:
+            annotations=json.load(f)
+        annotations[-1]["userlink"]="9a4b3f0e-7b55-40bf-9863-b862a5216765"
+        anno=Annotation(annotations)
+        anno.initialize()
+        assert anno.annotation_block_array[-1]["link"]=="9a4b3f0e-7b55-40bf-9863-b862a5216765"
+        annotations[-1]["userlink"]="nonexistentid"
+        anno=Annotation(annotations)
+        anno.initialize()
+        assert anno.annotation_block_array[-1]["link"]=="0d996604-014e-4ef1-ace2-0583ecc2eb70"
+
     
 
 if __name__ == '__main__':
