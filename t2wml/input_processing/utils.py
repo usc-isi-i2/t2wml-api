0ad1ad505e85c0d2501a97aa6d5fe73666f3efa1
devora <devorawitty@gmail.com>
2021-08-11 13:06:19 +0300

    remove ugly nested function

--- t2wml/input_processing/utils.py
+++ t2wml/input_processing/utils.py
@@ -1,8 +1,10 @@
 from string import punctuation
+
+def check_special_characters(text: str) -> bool:
+    return all(char in punctuation for char in str(text))
+
 def string_is_valid(text: str) -> bool:
     text=str(text)
-    def check_special_characters(text: str) -> bool:
-        return all(char in punctuation for char in str(text))
     if not text:
         return False
     text = text.strip().lower()
