f195d09535ef50119a61b0f65490d7163f938de1
devora <devorawitty@gmail.com>
2021-09-30 11:28:50 +0300

    move node creation to its own module

--- t2wml/api.py
+++ t2wml/api.py
@@ -7,7 +7,8 @@ from t2wml.mapping.statement_mapper import YamlMapper, StatementMapper, Annotati
 from t2wml.wikification.wikidata_provider import SparqlProvider, DictionaryProvider, WikidataProvider
 from t2wml.knowledge_graph import KnowledgeGraph, create_output_from_files
 from t2wml.project import Project
-from t2wml.input_processing.annotation_parsing import Annotation, AnnotationNodeGenerator, get_Pnode, get_Qnode
+from t2wml.input_processing.annotation_parsing import Annotation
+from t2wml.input_processing.node_creation import AnnotationNodeGenerator, get_Pnode, get_Qnode, create_nodes_from_selection
 from t2wml.input_processing.annotation_suggesting import block_finder, annotation_suggester
 
 
