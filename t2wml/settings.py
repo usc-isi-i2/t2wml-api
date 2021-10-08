246dddfb9eb85f731d2aa02def42ba229945abfb
devora <devorawitty@gmail.com>
2021-10-08 12:50:33 +0300

    document settings class

--- t2wml/settings.py
+++ t2wml/settings.py
@@ -3,6 +3,16 @@ import os
 DEFAULT_SPARQL_ENDPOINT= 'https://query.wikidata.org/bigdata/namespace/wdq/sparql'
 
 class T2WMLSettings:
+    """like utils.bindings, this is a class that is made globally accesible and used in many places for calculations.
+    It is therefore highly likely to be problematic in a multi-user server and an alternative should be found
+
+    Attributes:
+        sparql_endpoint (str): endpoint used to make sparql queries to get property type. note that with pre-cached properties this is less relevant
+        wikidata_provider (WikidataProvider): instance of class to be used to query property type.
+        warn_for_empty_cells (bool): block coordinate dictionary. taken from dictionary and then "normalized" (relabeled as necessary so that x1, y1 coordinates are upper left and x2, y2 lower right)
+        handle_calendar (str): when receiving a non-Gregorian calendar, should the statement generator: a. "leave" the original value as-is b. "replace" the value with the gregorian value c. "add" the gregorian as an additional dictionary
+        no_wikification (bool): treat item[] cells as value[], and do not run property validation
+    """
     def __init__(self):
         self.sparql_endpoint=DEFAULT_SPARQL_ENDPOINT
         self.wikidata_provider=None #default is DictionaryProvider with preloaded properties
