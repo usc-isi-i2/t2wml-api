b104596fb233670e1e6687ab01d29b00c036a246
devora <devorawitty@gmail.com>
2021-10-10 09:28:35 +0300

    add a warning about bindings.py

--- t2wml/utils/bindings.py
+++ t2wml/utils/bindings.py
@@ -1,4 +1,6 @@
-# bindings represents informationnused by classes/functions available in the t2wml parser
+# bindings represents information used by classes/functions available in the t2wml parser
+# This class is HIGHLY PROBLEMATIC for multi-user systems. It should be combined with T2WMLSettings, 
+# and some method of passing along both to the parser without using a global function needs to be implemented
 
 class BindingsClass:
     def __init__(self):
@@ -10,8 +12,7 @@ bindings = BindingsClass()
 
 
 def update_bindings(sheet=None, item_table=None) -> None:
-    """
-    This function updates the bindings dictionary with the excel_file, item_table, and sparql endpoint
+    """updates the bindings dictionary with the excel_file and item table
     """
     if sheet:
         bindings.excel_sheet = sheet
